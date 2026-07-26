9c2f51ac59ca73336e3660713f6ea0e206954b2f
Elif Sude Tuzlacı <chosusude@gmail.com>
2026-07-27 02:22:11 +0300

    27.07.2026

--- pb4.py
+++ pb4.py
@@ -22,3 +22,57 @@ print(a.intersection(b))
 
 print(a.difference(b))
 
+
+age= input("age:")
+
+print(int(age))
+
+
+price=input("price: ")
+print(float(price))
+
+sum= float(price) + (float(price)*18 /100)
+
+print("sum:", int(sum))
+
+
+#3
+
+numbers=[10,20,30,40,50]
+print(list[0])
+print(list[-1])
+print(list[1:])
+numbers.append(60)
+print(list)
+numbers.remove(20)
+print(numbers)
+
+#4
+
+tup=(12,34)
+x,y=tup
+
+print(x)
+print(y)
+
+#
+#numbers=(1,2,3,4,5,6)
+#numbers.append(8)
+#print(numbers)
+
+
+#5
+
+dict={"name":"Sude","Age":21,"department":"software"}
+
+print(dict["name"])
+dict["not"]=90
+dict["Age"]=22
+
+print(dict)
+
+#6
+names=["Altay","Ozlem","sude","suha"]
+unique_names=set(names)
+print(unique_names)
+print(len(unique_names))
---
+++ pb5.py
@@ -0,0 +1,41 @@
+"""
+pass
+
+"""
+if True:
+    #
+    pass
+
+for i in range(3):
+    if i==1:
+        pass
+    print(i)
+
+
+""""
+nested
+nested if
+for nested for 
+"""
+
+number=input("please enter a number: ")
+n=int(number)
+
+if(n>0):
+    print("numbers is positive")
+elif(n<0):
+    print("number is negative ")
+else:
+    print("number is zero")
+
+sum=0
+
+
+for i in range(1,11):
+    print(i)
+    sum=sum+i
+
+
+print(sum)
+
+
